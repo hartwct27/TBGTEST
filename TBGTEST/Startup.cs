033f24b0057693a9b18c817eec4d32874f757ee6
Hartono <555510170059@TBG>
2017-10-23 14:44:55 +0700

    Commit to the cloud

---
+++ TBGTEST/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TBGTEST.Startup))]
+namespace TBGTEST
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
